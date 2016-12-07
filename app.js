0333894d92df94baa2dd514fd00f5d0b1eb359bc
joshwilding4444 <joshwilding2011@gmail.com>
2016-12-07 13:41:20 -0700

    Fixed the problem with CSS not displaying.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var exphb = require('express-handlebars')
-
+var path = require('path')
 var app = express();
 
 app.engine('handlebars', exphb({defaultLayout: 'base' }))
 app.set('view engine', 'handlebars')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.render('index', {
